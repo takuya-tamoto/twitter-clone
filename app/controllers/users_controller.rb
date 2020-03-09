c68f290865b0b9683eb1ecd8f26296a4161cf3d8
takuya.tamoto <taku92860110@gmail.com>
2020-03-09 03:32:52 +0000

    likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,12 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+  def likes
+    @user = User.find(params[:id])
+    @favorites = @user.fav_microposts.page(params[:page])
+    counts(@user)
+  end
+  
   private
 
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,14 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
-  
+  #フォロー機能
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  #いいね機能
+  has_many :favorites
+  has_many :fav_microposts ,through: :favorites ,source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -32,4 +35,22 @@ class User < ApplicationRecord
     Micropost.where(user_id: self.following_ids + [self.id])
   end
   
+  def favorite(fav_micropost)
+    # お気に入りの中から、
+    # ①すでにお気に入りしているユーザーがある場合、そのお気に入りインスタンスを返す
+    # ②お気に入りしているユーザーがない場合、新たにお気に入りインスタンスをさ作成し、セーブする
+      self.favorites.find_or_create_by(micropost_id: fav_micropost.id)
+  end  
+    
+    # ユーザーがもつお気に入りの中から、
+    # ①消そうとしているお気に入りの投稿と同じmicroposts_idをもつお気に入りインスタンスをfavoriteに代入
+    # ②もし値が代入されていたら（nilじゃなければ）favorite を削除する
+  def unfavorite(fav_micropost)
+    favorite = self.favorites.find_by(micropost_id: fav_micropost.id)
+    favorite.destroy if favorite
+  end
+        
+  def favorite?(fav_micropost)
+    self.fav_microposts.include?(fav_micropost)
+  end
 end
